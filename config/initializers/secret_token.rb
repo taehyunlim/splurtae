199989a94d6bcd7fd549c3dd879af17637bdb1e0
Tae <taehyun.lim@gmail.com>
2016-02-14 01:03:30 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splurtae::Application.config.secret_key_base = '51d801614d9088ad18d6ef23564af1908dbb1e105ce888b6931b3680534cd5d786e9c2dca467c9845d09f5d9ff6b9c55b69453cc1bd3ea9f5b0525e11a406ffb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Splurtae::Application.config.session_store :cookie_store, key: '_splurtae_session'
